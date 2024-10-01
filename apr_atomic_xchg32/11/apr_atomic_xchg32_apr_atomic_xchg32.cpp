b7dfc18a444e9003c0b6848c1eea8d36aba88a07
VICCC <240268983@qq.com>
2024-10-01 18:48:58 +0800

    5

---
+++ apr_atomic_xchg32/11/apr_atomic_xchg32_apr_atomic_xchg32.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+
+/* Include files */
+#include "apr_atomic.h"
+/* End */
+
+#define ARG_NUM 1
+#define SIZE 10240
+
+int main(int argc, char **argv) {
+
+    /* Read and data from afl's input */
+
+    FILE* fp = fopen(argv[1], "r");
+    if(!fp) {
+        printf("Failed to open input file\n");
+        exit(-1);
+    }
+
+    /* Init variables */
+    unsigned int * val_0 = (unsigned int * )malloc(SIZE);
+    unsigned int val_1 = 0;
+    /* End Init */
+
+    /* Parse data */
+    char buffer[SIZE];
+    memset(buffer, 0, SIZE);
+    char* left = 0, *right = 0;
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%s", (char*)val_0);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%d", &val_1);
+            memset(buffer, 0, SIZE);
+        }
+    }
+    /* End Parse */
+
+    fclose(fp);
+    /* End */
+
+    /* Call API */
+    unsigned int ret_0 = apr_atomic_xchg32((unsigned int *)val_0, (unsigned int)val_1);
+
+    /* Well Done! */
+    return 0;
+}
