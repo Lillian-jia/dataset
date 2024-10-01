065135747f90f1e73b984142642645403151ec2a
meishijia <994291043@qq.com>
2024-10-01 18:52:40 +0800

    3-xuan

---
+++ apr_escape_json/11/apr_base64_apr_escape_json.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+
+/* Include files */
+#include "apr_escape.h"
+/* End */
+
+#define ARG_NUM 1
+#define SIZE 10240
+
+int main(int argc, char **argv) {
+
+    /* Read and data from afl's input */
+
+    FILE* fp = fopen(argv[1], "r");
+    if(!fp) {
+        printf("Failed to open input file\n");
+        exit(-1);
+    }
+
+    /* Init variables */
+    char * val_0 = (char * )malloc(SIZE);
+    char * val_1 = (char * )malloc(SIZE);
+    int val_2 = 0;
+    int val_3 = 0;
+    unsigned long * val_4 = (unsigned long * )malloc(SIZE);
+    /* End Init */
+
+    /* Parse data */
+    char buffer[SIZE];
+    memset(buffer, 0, SIZE);
+    char* left = 0, *right = 0;
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%s", (char*)val_0);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%s", (char*)val_1);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%d", &val_2);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%d", &val_3);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%s", (char*)val_4);
+            memset(buffer, 0, SIZE);
+        }
+    }
+    /* End Parse */
+
+    fclose(fp);
+    /* End */
+
+    /* Call API */
+    int ret_0 = apr_escape_json((char *)val_0, (char *)val_1, (int)val_2, (int)val_3, (unsigned long *)val_4);
+
+    /* Well Done! */
+    return 0;
+}
