3aa3e823217ccabe909a7e5d7daa64a8b45b7d45
Lillian-jia <865976635@qq.com>
2024-10-07 12:10:14 +0800

    qwj from group 4

---
+++ apr_siphash48/11/apr_siphash_48_apr_siphash48.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+
+/* Include files */
+#include "apr_siphash.h"
+/* End */
+
+#define ARG_NUM 1
+#define SIZE 10240
+
+int main(int argc, char **argv) {
+
+    /* Read and data from afl's input */
+
+    FILE* fp = fopen(argv[1], "r");
+    if(!fp) {
+        printf("Failed to open input file\n");
+        exit(-1);
+    }
+
+    /* Init variables */
+    const void * val_0 = (const void * )malloc(SIZE);
+    int val_1 = 0;
+    const unsigned char * val_2 = (const unsigned char * )malloc(SIZE);
+    /* End Init */
+
+    /* Parse data */
+    char buffer[SIZE];
+    memset(buffer, 0, SIZE);
+    char* left = 0, *right = 0;
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%s", (char*)val_0);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%d", &val_1);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%s", (char*)val_2);
+            memset(buffer, 0, SIZE);
+        }
+    }
+    /* End Parse */
+
+    fclose(fp);
+    /* End */
+
+    /* Call API */
+    long long ret_0 = apr_siphash48((const void *)val_0, (int)val_1, (const unsigned char *)val_2);
+
+    /* Well Done! */
+    return 0;
+}
