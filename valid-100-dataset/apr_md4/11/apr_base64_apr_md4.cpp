98e5ba0a8536b23d57d64e1cf589375ca1fa30de
ys <youngmith66@gmail.com>
2024-10-08 10:31:40 +0800

    描述你的更改

---
+++ valid-100-dataset/apr_md4/11/apr_base64_apr_md4.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+
+/* Include files */
+#include "apr_md4.h"
+/* End */
+
+#define ARG_NUM 1
+#define SIZE 10240
+
+int main(int argc, char **argv) {
+
+    /* Read and data from afl's input */
+
+    FILE* fp = fopen(argv[1], "r");
+    if(!fp) {
+        printf("Failed to open input file\n");
+        exit(-1);
+    }
+
+    /* Init variables */
+    unsigned char val_0 = 0;
+    char * val_1 = (char * )malloc(SIZE);
+    int val_2 = 0;
+    /* End Init */
+
+    /* Parse data */
+    char buffer[SIZE];
+    memset(buffer, 0, SIZE);
+    char* left = 0, *right = 0;
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%c", &val_0);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%s", (char*)val_1);
+            memset(buffer, 0, SIZE);
+        }
+    }
+
+    fgets(buffer, sizeof(buffer) - 1, fp);
+    left = strchr(buffer, '=');
+    if(left) {
+        right = strchr(left + 1, '=');
+        if(right) {
+            sscanf(right + 1, "%d", &val_2);
+            memset(buffer, 0, SIZE);
+        }
+    }
+    /* End Parse */
+
+    fclose(fp);
+    /* End */
+
+    /* Call API */
+    int ret_0 = apr_md4((unsigned char)val_0, (char *)val_1, (int)val_2);
+
+    /* Well Done! */
+    return 0;
+}
